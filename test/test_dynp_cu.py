0a521db24aa8d2edd542d045083a8c8c85ce15b2
aditya <adityaavinash143@gmail.com>
2014-06-19 12:51:01 -0400

    Added Dynamic Parallelism for CUDA

--- test/test_dynp_cu.py
+++ test/test_dynp_cu.py
@@ -3,26 +3,26 @@ import numpy as np
 
 @Urutu("CU")
 def math(a, b, c, d, e, f):
-        __shared is x , y
-        x = a[0:100]
-        y = b[0:100]
+	__shared is x , y
+	x = a[0:100]
+	y = b[0:100]
 
-        def add(p,q):
-                p = p + 1
-                return p+q
+	def add(p,q):
+		p = p + 1
+		return p+q
 
-        c[tx] = add(x[tx],y[tx])
+	c[tx] = add(x[tx],y[tx])
 
-        def sub(p,q,d):
-                p[tx] = 2*p[tx]
-                d[tx] = p[tx] - q[tx]
+	def sub(p,q,d):
+		p[tx] = 2*p[tx]
+		d[tx] = p[tx] - q[tx]
 
-        def saxpy(s,a,p,b,q):
-                s[tx] = a*p[tx] + b*q[tx]
+	def saxpy(s,a,p,b,q):
+		s[tx] = a*p[tx] + b*q[tx]
 
-        sub([100,1,1],[1,1,1],a,b,d)
-        saxpy(f,1.0345,x,-2,y)
-        return c, d, e, f
+	sub([100,1,1],[1,1,1],a,b,d)
+	saxpy(f,1.0345,x,-2,y)
+	return c, d, e, f
 
 a = np.random.randint(10, size = 100)
 b = np.random.randint(10, size = 100)
