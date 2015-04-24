82b249c4ba45ee13d075a884d0cd642ed41ad00e
Aditya Avinash Atluri <adityaavinash1@gmail.com>
2015-04-24 13:15:02 -0400

    Update test_lazy.py

--- test/test_lazy.py
+++ test/test_lazy.py
@@ -6,13 +6,9 @@ def math(a, b, c, d, e, f):
 	__shared is x , y
 	x[tx] = a[tx]
 	y[tx] = b[tx]
-
 	c[tx] = x[tx] + y[tx] + 1
-
 	d[tx] = 2*x[tx] - y[tx]
-
 	f[tx] = 1.0345*x[tx] - 2*y[tx]
-
 	return c, d, e, f
 
 a = np.random.randint(10, size = 100)
@@ -24,5 +20,4 @@ f = np.array(a, dtype = 'd')
 
 print "The Array A is: \n", a
 print "The Array B is: \n", b
-#print "", math(True, a,b,c,d,e,f)
 print "Running on GPU.. \n", math([100,1,1],[1,1,1],a, b, c, d, e, f)
